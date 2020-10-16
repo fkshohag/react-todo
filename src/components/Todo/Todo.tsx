e60c63ff942fa6baa8205252edac7f47224362f2
Fazlul Kabir Shohag <linkvisionsoft@yahoo.com>
2020-10-17 01:14:23 +0600

    update

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,38 +1,51 @@
 import React from 'react';
-import styles from './Todo.module.scss'
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import styles from './Todo.module.scss';
 import Container from '@material-ui/core/Container';
 import { Button } from '@material-ui/core';
+import TableContainer from '@material-ui/core/TableContainer';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import TableBody from '@material-ui/core/TableBody';
+import Paper from '@material-ui/core/Paper';
 
-class Todo extends React.Component {
-  render() {
-    return <Container>
-      <div>
-        <Button style={{textAlign: 'left'}} variant="contained" color="primary">Add To-do</Button>
-        <Button style={{textAlign: 'left'}} variant="contained" color="secondary">Import Json</Button>
+const Todo: React.FC = () => (
+  <div className={styles.Todo} data-testid="Todo">
+    <Container className={styles.Todo}>
+      <div className={styles.buttonAlignment}>
+        <Button variant="contained" color="primary">Add To-do</Button>
+        <Button variant="contained" color="secondary">Import Json</Button>
       </div>
       <TableContainer component={Paper}>
-        <Table className={styles.table} aria-label="simple table">
+        <Table className={styles.table}>
           <TableHead>
             <TableRow>
-              <TableCell>To Do</TableCell>
-              <TableCell align="right">In progress</TableCell>
-              <TableCell align="right">Done</TableCell>
+              <TableCell className={styles.textAlign}>To Do</TableCell>
+              <TableCell className={styles.textAlign} align="right">In progress</TableCell>
+              <TableCell className={styles.textAlign} align="right">Done</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-
+            <TableRow>
+              <TableCell className={styles.ColumnStyle}>
+                <p>sdf</p>
+                <p>sdf</p>
+                <p>sdf</p>
+                <p>sdf</p>
+                <p>sdf</p>
+                <p>sdf</p>
+                <p>sdf</p>
+                <p>sdf</p>
+              </TableCell>
+              <TableCell className={styles.ColumnStyle}>SD</TableCell>
+              <TableCell className={styles.ColumnStyle}>SD</TableCell>
+            </TableRow>
           </TableBody>
         </Table>
       </TableContainer>
     </Container>
-  }
-}
+  </div>
+);
 
 export default Todo;
