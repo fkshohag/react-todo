2ee904e08d7c821d8ac0dc38b7f5b648294fb28e
Fazlul Kabir Shohag <linkvisionsoft@yahoo.com>
2020-10-17 00:40:41 +0600

    update

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import styles from './Todo.module.scss'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container';
+import { Button } from '@material-ui/core';
 
 class Todo extends React.Component {
   render() {
-    return <div>
-      <h1>Hello man!</h1>
-    </div>
+    return <Container>
+      <div>
+        <Button style={{textAlign: 'left'}} variant="contained" color="primary">Add To-do</Button>
+        <Button style={{textAlign: 'left'}} variant="contained" color="secondary">Import Json</Button>
+      </div>
+      <TableContainer component={Paper}>
+        <Table className={styles.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>To Do</TableCell>
+              <TableCell align="right">In progress</TableCell>
+              <TableCell align="right">Done</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   }
 }
 
